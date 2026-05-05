608ca901e7001d6d0bb37a02bf29892e325f80f1
Sameer <sam.dev1611@gmail.com>
2026-05-05 21:57:54 +0530

    Initial commit - Added Jarvis Voice

---
+++ main.py
@@ -0,0 +1,14 @@
+import speech_recognition as sr
+import os
+
+def say(text):
+    os.system(
+        f'powershell -Command "Add-Type -AssemblyName System.Speech; ' # For Window
+        f'$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; '
+        f'$speak.Volume = 75; '   # 🔉 0 to 100 
+        f'$speak.Speak(\'{text}\')"'
+    )
+
+if __name__ == '__main__':
+    print('PyCharm')
+    say('I am Jarvis AI')
