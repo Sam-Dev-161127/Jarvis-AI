f091d1489cbbe6341c13f3017dea50f3028a366e
Sameer <sam.dev1611@gmail.com>
2026-05-06 20:51:05 +0530

    Now Jarvis can listen to the user

--- main.py
+++ main.py
@@ -3,12 +3,28 @@ import os
 
 def say(text):
     os.system(
-        f'powershell -Command "Add-Type -AssemblyName System.Speech; '  # Load speech lib
-        f'$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; '  # Create voice engine
-        f'$speak.Volume = 75; '   # Set volume (0-100)
+        f'powershell -Command "Add-Type -AssemblyName System.Speech; '
+        f'$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; '
+        f'$speak.Volume = 75; '
         f'$speak.Speak(\'{text}\')"'
     )
 
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        try:
+            query = r.recognize_google(audio, language='en-in')
+            print(f"User said: {query}")
+            return query
+        except Exception as e:   # ✅ FIXED INDENTATION
+            return "Some Error Occured,Sorry from Jarvis"
+
 if __name__ == '__main__':
-    print('PyCharm')  # Console test
-    say('I am Jarvis AI made by Sam')  # Speak text
+    print('PyCharm')
+    say('I am Jarvis AI made by Sam')
+    while True:
+        print("Listening...")
+        text = takeCommand()
+        say(text)
